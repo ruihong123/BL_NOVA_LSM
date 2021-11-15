cdd6b8d7d7d21d60f09dbc1ab1c9b0c56f8af85a
ruihong123 <wang4996@purdue.edu>
2021-11-14 22:10:11 -0500

    make it work on cloudlab

--- novalsm/rdma_server.cpp
+++ novalsm/rdma_server.cpp
@@ -647,7 +647,7 @@ namespace nova {
         }
 //        NOVA_LOG(INFO) << "*** <End> The received buffer[0] for rdma-server is "<< buf[0];
 //        NOVA_LOG(INFO) << "*** <End> type is "<< type;
-        NOVA_LOG(INFO) << "*** <End> processed is "<< processed;
+//        NOVA_LOG(INFO) << "*** <End> processed is "<< processed;
         return processed;
     }
 }
