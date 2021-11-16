fb8103fdf4b244f9a1dcbdf3df8d62b431d3a9f5
ruihong123 <wang4996@purdue.edu>
2021-11-16 16:56:32 -0500

    make it work on cloudlab

--- db/builder.cc
+++ db/builder.cc
@@ -106,6 +106,7 @@ namespace leveldb {
             Slice user_key;
             bool insert = true;
             int total_kv_num = 0;
+            int dropped_num = 0;
             meta->smallest.DecodeFrom(iter->key());
             for (; iter->Valid(); iter->Next()) {
                 insert = true;
@@ -114,6 +115,7 @@ namespace leveldb {
                     Slice ukey = ExtractUserKey(key);
                     if (!user_key.empty() && user_comp->Compare(ukey, user_key) == 0) {
                         insert = false;
+                        dropped_num++;
                     }
                     user_key = ukey;
                 }
@@ -125,10 +127,11 @@ namespace leveldb {
             }
             NOVA_LOG(rdmaio::INFO)
                 << fmt::format(
-                        "!!!!!!!!!!!!!!!!!!!!! CompactMemTable tid:{} alloc_size:{} nentries:{} nblocks:{}, total_kv_num:{}",
+                        "!!!!!!!!!!!!!!!!!!!!! CompactMemTable tid:{} alloc_size:{} nentries:{} "
+                        "nblocks:{}, total_kv_num:{}, dropped_num{}",
                         bg_thread->thread_id(), options.max_stoc_file_size,
                         builder->NumEntries(),
-                        builder->NumDataBlocks(), total_kv_num);
+                        builder->NumDataBlocks(), total_kv_num, dropped_num);
 
             // Finish and check for builder errors
             s = builder->Finish();
